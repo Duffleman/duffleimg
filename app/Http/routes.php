8b3dabcf643e01389d681dcfc0b26b911f102dc8
George Miller <git@duffleman.co.uk>
2017-07-29 11:49:57 +0100

    new route for GUI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,5 @@
 <?php
 
-$app->get('/', function () use ($app) {
-    return response()->json([
-        'message' => 'Hello World!'
-    ]);
-});
-
+$app->get('/', function () use ($app) { return view('upload'); });
 $app->post('/', 'UploadController@handle');
 $app->get('{hash}', 'MainController@index');
